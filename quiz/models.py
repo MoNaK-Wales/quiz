eb474b1ad34ec47c07371071ab4d1cbd4ace67a7
Maksim <felozan4ik2007@gmail.com>
2025-09-25 05:26:59 +0200

    quiz models

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    creator = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True, related_name='quizzes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_public = models.BooleanField(default=True)
+    play_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')
+    text = models.TextField()
+    image = models.ImageField(upload_to='questions/', null=True, blank=True)
+    video = models.FileField(upload_to='questions/', null=True, blank=True)
+    time_limit = models.IntegerField(default=30)
+
+    def __str__(self):
+        return self.text[:50]
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+    
